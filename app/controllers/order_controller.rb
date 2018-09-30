bd343cc09994d2c62544fac12bd92b1cd68d9502
nesschicha6 <nesschicha6@gmail.com>
2018-09-30 13:09:30 -0400

    routesv1

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,36 @@
+class OrderController < ApplicationController
+        skip_before_action :verify_authenticity_token, :only => [:create]
+  
+def create 
+    @name = params[:name]
+    @adress = params[:adress]
+    @pizzas = params[:pizzas]
+    newOrder = Order.new(:name => @name, :adress => @adress)
+    newOrder.save
+    @pizzas.each do |pizza|
+      @orderPizza =Pizza.where(name: pizza).first
+      newOrderline = Orderline.new
+      newOrderline.order=newOrder
+      newOrderline.pizza=@orderPizza
+      newOrderline.save
+    end
+   
+     render json: {"message :" => "Order created"}
+end
+  
+   def view
+    @orders= []
+    @allOrders = Order.all
+    @allOrders.each do |order|
+      pizzas = []
+      @orderlines=Orderline.where(order: order)
+      @orderlines.each do |orderline|
+          pizza = Pizza.find(orderline[:pizza_id])
+          pizzas.push(pizza)
+      end
+      @orders.push({order: order ,pizzas: pizzas})
+    end
+end
+  
+  
+end
